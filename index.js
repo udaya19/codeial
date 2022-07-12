18d9727df6d818b006b1ccc3e0eeb891e26287cf
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-07-12 12:31:46 +0530

    Set up ejs view enjine

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 3000
 //use express router
 app.use('/',require('./routes/index'))
-
+//set up view engine
+app.set('view engine','ejs');
+app.set('views','./views')
 app.listen(port,(err)=>{
     if(err){
         console.log("Error:",err)
