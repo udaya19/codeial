d44403de8fb6153d6b225e721768abf34fb71829
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-07-18 10:13:52 +0530

    Css for layouts

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 app.use(expressLayouts);
+app.use(express.static('./assests'));
 const port = 3000
 //use express router
 app.use('/',require('./routes/index'))
+//Extract styles and scripts from sub pages into layouts
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 //set up view engine
 app.set('view engine','ejs');
 app.set('views','./views')
