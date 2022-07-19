db331548311521e78e83f58646f1621892f48c3d
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-07-19 12:08:07 +0530

    Added routes for sign in and sign up

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -3,3 +3,24 @@ module.exports.profile = function(req,res){
         title:"User Profile"
     });
 }
+
+module.exports.signUp = function(req,res){
+    return res.render('user_signup.ejs',{
+        title:"User SignUp"
+    });
+}
+
+module.exports.signIn = function(req,res){
+    return res.render('user_signin.ejs',{
+        title:"User SignIn"
+    });
+}
+
+//get sign up data
+module.exports.create = (req,res)=>{
+    //TODO later
+}
+//signin and create a session for user
+module.exports.createSession = (req,res)=>{
+    //TODO later
+}
--- routes/users.js
+++ routes/users.js
@@ -4,4 +4,6 @@ const router = express.Router();
 
 const usersController = require('../controllers/user_controller');
 router.get('/',usersController.profile)
+router.get('/sign-up',usersController.signUp);
+router.get('/sign-in',usersController.signIn)
 module.exports = router;
