c4a5545be616d423c1c8a0e18f43f89a579c5d03
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-07-19 12:49:53 +0530

    Creating new user

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,4 @@
+const User = require('../models/user')
 module.exports.profile = function(req,res){
     return res.render('user.ejs',{
         title:"User Profile"
@@ -18,7 +19,28 @@ module.exports.signIn = function(req,res){
 
 //get sign up data
 module.exports.create = (req,res)=>{
-    //TODO later
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back')
+    }
+    User.findOne({email:req.body.email},(err,user)=>{
+        if(err){
+            console.log("Error in finding user in sigining in up");
+            return
+        }
+        if(!user){
+            User.create(req.body,(err,user)=>{
+                if(err){
+                    console.log("Error in finding user in sigining up");
+                    return;
+                }
+                return res.redirect('/users/sign-in');
+            })
+        }
+        else{
+            return res.redirect('back')
+        }
+    })
+
 }
 //signin and create a session for user
 module.exports.createSession = (req,res)=>{
--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const usersController = require('../controllers/user_controller');
 router.get('/',usersController.profile)
 router.get('/sign-up',usersController.signUp);
-router.get('/sign-in',usersController.signIn)
+router.get('/sign-in',usersController.signIn);
+router.post('/create',usersController.create);
 module.exports = router;
