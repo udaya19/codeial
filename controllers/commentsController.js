74f02f16225330a5068fa43203c7ab719892f4b8
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-08-01 15:46:01 +0530

    Changes w.r.t comments control

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -1,34 +1,37 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
-module.exports.create = (req,res)=>{
-    Post.findById(req.body.post,(err,post)=>{
-        if(post){
-            Comment.create({
-                content:req.body.content,
-                post:req.body.post,
-                user:req.body.user
-            },(err,comment)=>{
-                post.comments.push(comment);
-                post.save();
-                res.redirect('/');
-            })
-        }
+const commentsMailer = require('../mailers/comment_mailer');
+module.exports.create = async (req,res)=>{
+    let post = await Post.findById(req.body.post);
+    if(post){
+       let comment = await Comment.create({
+            content:req.body.content,
+            post:req.body.post,
+            user:req.body.user
+        })
+        post.comments.push(comment);
+        post.save();
+        comment = await comment.populate('user','name email')
+        res.redirect('back')
+    }
 
-    })
 }
 
-module.exports.destroy = (req,res)=>{
+module.exports.destroy = async (req,res)=>{
     // console.log(" k,")
-    Comment.findById(req.params.id,(err,comment)=>{
-        if(comment.user == req.user.id){
-            let postId = comment.post
-            comment.remove();
-            Post.findByIdAndUpdate(postId,{$pull:{comments:req.params.id}},(err,post)=>{
-                return res.redirect('back')
-            })
-        }
-        else{
-            return res.redirect('back')
-        }
-    })
+   try {
+    let comment = await Comment.findById(req.params.id);
+    if(comment.user == req.user.id){
+        let postId = comment.post
+        // await Comment.deleteOne({id:comment.id})
+        comment.remove()
+        let post = await Post.findByIdAndUpdate(postId,{$pull:{comments:req.params.id}});
+    }
+    else{
+        return res.redirect('back')
+    }
+   }
+   catch (error) {
+    console.log('Error');
+   }
 }
