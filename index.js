6899ecfeacdae3e71b59ca3595340d17cd6660d8
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2022-08-04 13:19:33 +0530

    Setting socket.io

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
+//setup chatserver to use with socket.io
+const chatServer = require('http').Server(app);
+const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
+chatServer.listen(5000);
+console.log("Chat server is listening on port 5000");
+
 app.use(saasMiddleware({
     src:"./assests/scss",
     dest:"./assests/css",
